52efa7a429ebca2a664f8104ffdc47382b6050f9
Ian Adams <ijadams503@gmail.com>
2019-08-21 10:24:49 +0200

    write JSON string to file

--- fry-scraper.js
+++ fry-scraper.js
@@ -12,22 +12,24 @@ axios.get(url)
         const $ = cheerio.load(html);
         const body = $('.asset #asset-content p');
         // skip first 2 and last 2 p tags as they are not locations
-        for (let i = 2; i < body.length; i++) {
+        for (let i = 3; i < body.length; i++) {
             const text = $(`.asset #asset-content p:nth-of-type(${i})`).text().toString();
             const obj = {
-                title: text.match('^[^:]*')[0],
-                street: text.match(/\d{1,3}.?\d{0,3}\s[a-zA-Z]{2,30}\s[a-zA-Z]{2,15}/)[0],
-                startTime: text.match(/\d{1,2}(\.\d{1,2})? ?([a|p].m.|noon)/)[0],
-                endTime: text.match(/\d{1,2}(\.\d{1,2})? ?([a|p].m.,|noon,)/)[0],
-                everyFriday: text.toLowerCase().match('every friday').length,
+                title: text.match('^[^:]*') ? text.match('^[^:]*')[0] : null,
+                street: text.match(/\d{1,3}.?\d{0,3}\s[a-zA-Z]{2,30}\s[a-zA-Z]{2,15}/) ? text.match(/\d{1,3}.?\d{0,3}\s[a-zA-Z]{2,30}\s[a-zA-Z]{2,15}/)[0] : null,
+                startTime: text.match(/\d{1,2}(\.\d{1,2})? ?([a|p].m.|noon)/) ? text.match(/\d{1,2}(\.\d{1,2})? ?([a|p].m.|noon)/)[0] : null,
+                endTime: text.match(/\d{1,2}(\.\d{1,2})? ?([a|p].m.,|noon,)/) ? text.match(/\d{1,2}(\.\d{1,2})? ?([a|p].m.,|noon,)/)[0] : null,
+                everyFriday: text.toLowerCase().match('every friday') ? text.toLowerCase().match('every friday').length > 0 : false,
                 dates: text.match(/(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+(\d{1,2})[^.]+[.]/),
                 telephone: text.match(/(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?/),
                 email: text.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
             };
             data.push(obj);
         }
-        const json = JSON.stringify(data);
+        let json = {data: data};
+        json = JSON.stringify(json);
         fs.writeFile('fish-fry.json', json, 'utf8', (err) => {
-            console.log(err)
+            return err;
         });
+        console.log('*** fish fries scraped 🤘 ***');
     }, err => console.log(err));
